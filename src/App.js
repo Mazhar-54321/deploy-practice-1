8f64457dc0a36b56eb0f7b0b1d4fd1100b0f0498
MazharAliFacctum <syed.ali@facctum.com>
2024-02-17 20:22:53 +0530

    MAde Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import SampleDrawer from './SampleDrawer';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: '#FFFFFF'
+    }
+  },
+});
 function App() {
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Papa
-        </a>
-      </header>
+      
+      <ThemeProvider theme={theme}>
+         <SampleDrawer /> 
+        
+      
+      </ThemeProvider>
     </div>
   );
 }
